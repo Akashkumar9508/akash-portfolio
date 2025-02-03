a711497d7792b952c44b9e38446e2ff0eb099ed1
Akashkumar9508 <akashbca9508@gmail.com>
2025-02-03 23:11:39 +0530

    text animation added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import AboutHero from "@/components/about/herosection";
+import Experience from '@/components/about/Experience';
 
 const page = () => {
   return (
     <div>
       <AboutHero />
+      <Experience />
     </div>
   )
 }
---
+++ src/components/utility/FlipWords.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FlipWords } from "../ui/flip-words";
+
+const FlipWordsDemo=()=> {
+  const words = ["Web Developer", "UI Designer", "Creator"];
+
+  return (
+    <>
+        <FlipWords words={words} /> 
+    </>
+      
+  );
+}
+export default FlipWordsDemo
