266bfe193e82d0de6affb9703a96c81ccc843803
Akashkumar9508 <akashbca9508@gmail.com>
2025-02-09 20:54:29 +0530

    footer added

--- src/components/utility/Footer.tsx
+++ src/components/utility/Footer.tsx
@@ -5,10 +5,10 @@ import FadeUp from '@/animations/fade-up'
 const Footer = () => {
   return (
     <FadeUp duration={1.2}  whileInView>
-    <div className='h-[7vh] w-full flex gap-4 justify-center items-center' >
-        <div className='w-[30%] h-[0.2vh] bg-white'> </div>
-        <h1>made with ❤️ Akash</h1>
-        <div className='w-[30%] h-[0.2vh] bg-white'> </div>
+    <div className='h-[7vh]  w-full flex gap-4 justify-center items-center' >
+        <div className='w-[20%] md:w-[30%] h-[0.2vh] bg-black dark:bg-white'> </div>
+        <h1 className='text-[3vw] md:text-[1vw]' >made with ❤️ <span className='text-[--mainText] dark:text-[--mainText]' >Akash</span></h1>
+        <div className=' w-[20%] md:w-[30%] h-[0.2vh] bg-black dark:bg-white'> </div>
     </div>
     </FadeUp>
   )
