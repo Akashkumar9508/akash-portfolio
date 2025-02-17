7afe552aaa90e1c62d49e6bb92515be08bcf7918
Akashkumar9508 <akashbca9508@gmail.com>
2025-02-17 19:48:51 +0530

    skill effect added

--- src/components/skill/TextShadow.tsx
+++ src/components/skill/TextShadow.tsx
@@ -2,15 +2,31 @@
 
 import { LineShadowText } from "@/components/ui/textShadow";
 import { useTheme } from "next-themes";
+import { useState, useEffect } from "react";
 
 export function LineShadowTextDemo() {
-  const theme = useTheme();
-  const shadowColor = theme.resolvedTheme === "dark" ? "white" : "black";
+  const { resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <h1 className="project-heading text-transparent">Loading...</h1>;
+  }
+
+  const shadowColor = resolvedTheme === "dark" ? "white" : "black";
+
   return (
-    <h1 className="project-heading text-black dark:text-white text-[8vh] sm:text-[8vh] mb-24 sm:mb-28 md:text-center " >Skills I
-    <LineShadowText className=" italic text-[--mainText] dark:text-[--mainText] font-bold" shadowColor={shadowColor}>
-    KNOW
-     </LineShadowText>
+    <h1 className="project-heading text-black dark:text-white text-[8vh] sm:text-[8vh] mb-24 sm:mb-28 md:text-center">
+      Skills I{" "}
+      <LineShadowText
+        className="italic text-[--mainText] dark:text-[--mainText] font-bold"
+        shadowColor={shadowColor}
+      >
+        KNOW
+      </LineShadowText>
     </h1>
   );
 }
