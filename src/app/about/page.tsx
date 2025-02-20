66f639b451e8707409221ed2eb9e7a688e6424b6
Akashkumar9508 <akashbca9508@gmail.com>
2025-02-21 01:00:43 +0530

    added about and icon

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,19 +3,26 @@ import React from 'react'
 import AboutHero from "@/components/about/herosection";
 import TimelineDemo from "@/components/about/TimeLine"
 import { TerminalDemo } from '@/components/about/Terminal';
-import {Form} from "@/components/utility/Form"
+import { Form } from "@/components/utility/Form"
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 const page = () => {
   return (
     <>
       <div className="div px-4 ">
-        <div className="div w-full sm:h-[90vh] gap-4 h-auto flex md:flex-row flex-col jc items-center">
+        <div className="div w-full sm:h-[90vh] gap-4 h-[80vh] flex md:flex-row flex-col jc items-center">
 
           <div className="aboutHero w-full md:w-1/2 h-full ">
+            <DotLottieReact
+              src="https://lottie.host/45a49eb4-59f3-493e-ad68-00c1d52e08d5/axSgYW87xb.lottie"
+              loop
+              autoplay
+            />
+
 
           </div>
           <div className="abouterminal w-full md:w-1/2 h-full flex items-center justify-left  ">
-        <TerminalDemo />
+            <TerminalDemo />
           </div>
 
         </div>
